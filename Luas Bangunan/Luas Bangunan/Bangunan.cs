813d77a008f13eff335ac85fe415f6ed696ffccd
erick-alex-sander <55838569+erick-alex-sander@users.noreply.github.com>
2020-08-12 13:29:48 +0700

    Upload Luas Bangunan

---
+++ Luas Bangunan/Luas Bangunan/Bangunan.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luas_Bangunan
+{
+    // Abstraction
+    public abstract class Bangunan
+    {
+        // Abstraction
+        public abstract double Area();
+        public abstract double Length();
+    }
+
+    // Inheirtance
+    public class Bulat : Bangunan
+    {
+        const double Pi = 3.14;
+        // Encapsulation
+        public double radius { get; set; }
+
+        // Constructor
+        public Bulat(double radius)
+        {
+            this.radius = radius;
+        }
+
+        // Polymorphism
+        public override double Area()
+        {
+            return radius * radius * Pi;
+        }
+
+        // Polymorphism
+        public override double Length()
+        {
+            return 2 * Pi * radius;
+        }
+
+        // Static
+        public static void Print()
+        {
+            Console.WriteLine("Luas dan Keliling Lingkaran Masing - Masing :");
+        }
+    }
+
+    // Inheirtance
+    public class Persegi : Bangunan
+    {
+        // Encapsulation
+        public double side { get; set; }
+
+        // Constructor
+        public Persegi(double side)
+        {
+            this.side = side;
+        }
+
+        // Polymorphism
+        public override double Area()
+        {
+            return side * side;
+        }
+
+        // Polymorphism
+        public override double Length()
+        {
+            return 4 * side;
+        }
+
+        // Static
+        public static void Print()
+        {
+            Console.WriteLine("Luas dan Keliling Persegi Masing - Masing :");
+        }
+    }
+
+    // Inheirtance
+    public class PersegiPanjang : Persegi
+    {
+        // Encapsulation
+        public double height { get; set; }
+
+        // Constructor
+        public PersegiPanjang(double side, double height) : base(side)
+        {
+            this.side = side;
+            this.height = height;
+        }
+
+        // Polymorphism
+        public override double Area()
+        {
+            return side * height;
+        }
+
+        // Polymorphism
+        public override double Length()
+        {
+            return 2 * (side + height);
+        }
+
+        // Static
+        public static void Print()
+        {
+            Console.WriteLine("Luas dan Keliling Persegi Panjang Masing - Masing :");
+        }
+    }
+}
---
+++ Luas Bangunan/Luas Bangunan/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Luas_Bangunan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continues;
+            try
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Pilih Bangunan : ");
+                    Console.WriteLine("1. Bulat");
+                    Console.WriteLine("2. Persegi");
+                    Console.WriteLine("3. Persegi Panjang");
+
+                    int answer = Convert.ToInt16(Console.ReadLine());
+
+                    switch (answer)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Insert your radius");
+                            double input = Convert.ToDouble(Console.ReadLine());
+                            // Instance
+                            Bulat l1 = new Bulat(input);
+                            Bulat.Print();
+                            Console.WriteLine(l1.Area() + " " + l1.Length());
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Insert your side");
+                            input = Convert.ToDouble(Console.ReadLine());
+                            // Instance
+                            Persegi p1 = new Persegi(input);
+                            Persegi.Print();
+                            Console.WriteLine(p1.Area() + " " + p1.Length());
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("Insert your length and height");
+                            double length = Convert.ToDouble(Console.ReadLine());
+                            double height = Convert.ToDouble(Console.ReadLine());
+                            // Instance
+                            PersegiPanjang pp1 = new PersegiPanjang(length, height);
+                            PersegiPanjang.Print();
+                            Console.WriteLine(pp1.Area() + " " + pp1.Length());
+                            break;
+                        default:
+                            Console.WriteLine("Wrong input.");
+                            break;
+                    }
+
+                    Console.WriteLine("Do you want to continue? (Press y to continue)");
+                    continues = Console.ReadLine();
+                } while (continues.ToLower() == "y") ;           
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Wrong Input");
+            }
+            
+            catch (OverflowException)
+            {
+                Console.WriteLine("Too big");
+            }
+        }
+    }
+}
